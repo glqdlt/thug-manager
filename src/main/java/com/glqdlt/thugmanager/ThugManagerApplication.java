97efc92c0b8afc3658faf64a7bdc21825f27e00f
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-03-26 11:39:20 +0900

    Project init

---
+++ src/main/java/com/glqdlt/thugmanager/ThugManagerApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.thugmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThugManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThugManagerApplication.class, args);
+	}
+}
