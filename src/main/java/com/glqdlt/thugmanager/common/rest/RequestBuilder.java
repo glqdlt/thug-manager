f7069d14e692e517f5428472725002a80eaffb6f
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-03-26 14:34:53 +0900

    async restteample

--- src/main/java/com/glqdlt/thugmanager/common/rest/RequestBuilder.java
+++ src/main/java/com/glqdlt/thugmanager/common/rest/RequestBuilder.java
@@ -1,9 +1,16 @@
 package com.glqdlt.thugmanager.common.rest;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.AsyncRestTemplate;
+
 /**
  * Created By iw.jhun
  * On 2018-03-26 , 오전 11:42
  */
 
+@Component
 public class RequestBuilder {
+
+    public final static AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate();
+
 }
--- src/test/java/com/glqdlt/thugmanager/common/rest/RequestBuilderTest.java
+++ src/test/java/com/glqdlt/thugmanager/common/rest/RequestBuilderTest.java
@@ -4,15 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.AsyncRestTemplate;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,31 +26,52 @@ import java.util.concurrent.TimeUnit;
 public class RequestBuilderTest {
 
 
+    @Autowired
+    RequestBuilder requestBuilder;
+
     AsyncRestTemplate asyncRestTemplate;
 
     @Before
-    public void init(){
-        this.asyncRestTemplate = new AsyncRestTemplate();
+    public void init() {
+        this.asyncRestTemplate = requestBuilder.asyncRestTemplate;
     }
 
     @Test
     public void asyncRestTemplateTest() throws InterruptedException {
 
 
-        ListenableFuture<ResponseEntity<String>> future = this.asyncRestTemplate.getForEntity("http://www.google.co.kr",
-                String.class);
+        List<String> URLS = new ArrayList<>();
+        URLS.add("http://www.google.co.kr");
+        URLS.add("http://www.daum.net");
+        URLS.add("http://www.naver.com");
+
+        List<ListenableFuture<ResponseEntity<String>>> listFuture = new ArrayList<>();
+
+        URLS.forEach(x -> {
+            listFuture.add(callbackMethod(x));
+        });
+
+
+
+        TimeUnit.SECONDS.sleep(15);
+
+    }
+
+
+    public ListenableFuture<ResponseEntity<String>>  callbackMethod(String url) {
+
+        ListenableFuture<ResponseEntity<String>> future = this.asyncRestTemplate.getForEntity(url, String.class);
 
         future.addCallback(result -> {
             log.debug("=================");
+            log.debug("url: "+url);
             log.debug(result.getBody());
-            log.debug(result.getStatusCode()+"");
+            log.debug(result.getStatusCode() + "");
             log.debug("=================");
         }, e -> {
-            log.error(e.getMessage(),e);
-        } );
-
-        TimeUnit.SECONDS.sleep(8);
-
+            log.error(e.getMessage(), e);
+        });
+        return future;
     }
 
 }
